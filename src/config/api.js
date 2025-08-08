7c7d1f3eae396018a084d591a7f0cdd7d5e8a137
Juancolque04 <jdiazcolque04@gmail.com>
2025-08-08 12:53:08 -0300

    ya

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 // Configuración base de la API
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api"
+const API_BASE_URL = import.meta.env.VITE_API_URL ||  "https://api.miniservice24.site/api"
+
 
 // NUEVO: Cache simple para requests GET
 const requestCache = new Map()
